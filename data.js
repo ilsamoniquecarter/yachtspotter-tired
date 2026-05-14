30f96f2ee97d6cd7411939e457b12e68fae58150
ilsamoniquecarter <168980197+ilsamoniquecarter@users.noreply.github.com>
2026-05-14 10:40:44 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,105 @@
+const yachts = [
+
+  { name:"Azzam", tags:["huge","white","mega","long","royal"] },
+
+  { name:"Eclipse", tags:["dark","helicopter","large","sleek","security"] },
+
+  { name:"Dilbar", tags:["wide","white","volume","massive","luxury"] },
+
+  { name:"Flying Fox", tags:["charter","white","spa","modern","balearic"] },
+
+  { name:"Kaos", tags:["glass","pool","wide","modern","ibiza"] },
+
+  { name:"Savannah", tags:["green","hybrid","unique","feadship","tech"] },
+
+  { name:"Aviva", tags:["grey","sport","modern","court","luxury"] },
+
+  { name:"Kismet", tags:["dark","sharp","luxury","modern","lürssen"] },
+
+  { name:"Seven Seas", tags:["white","clean","cinema","large","hollywood"] },
+
+  { name:"Lady Moura", tags:["gold","classic","icon","traditional","palma"] },
+
+  { name:"Carinthia VII", tags:["white","classic","lürssen","palma","frequent"] },
+
+  { name:"Madsummer", tags:["white","oceanco","modern","luxury","balearic"] },
+
+  { name:"Elements", tags:["helipad","sleek","modern","white","charter"] },
+
+  { name:"Serene", tags:["massive","white","royal","luxury","mega"] },
+
+  { name:"Nord", tags:["explorer","white","modern","huge","steel"] },
+
+  { name:"Black Pearl", tags:["black","sailing","futuristic","unique","eco"] },
+
+  { name:"Sailing Yacht A", tags:["black","giant","sailing","unusual","iconic"] },
+
+  { name:"Koru", tags:["bezos","white","huge","sailing","support"] },
+
+  { name:"Rising Sun", tags:["white","large","entertainment","classic","geffen"] },
+
+  { name:"Octopus", tags:["explorer","research","submarine","iconic","expedition"] },
+
+  { name:"Faith", tags:["white","spa","luxury","resort","feadship"] },
+
+  { name:"Bravo Eugenia", tags:["sleek","grey","modern","ocean","nfl"] },
+
+  { name:"Al Lusail", tags:["royal","white","massive","qatar","security"] },
+
+  { name:"Opera", tags:["modern","white","giant","abudhabi","new"] },
+
+  { name:"Luna", tags:["steel","explorer","modern","security","oligarch"] },
+
+  { name:"Solaris", tags:["white","explorer","huge","modern","abramovich"] },
+
+  { name:"Vava II", tags:["white","classic","charter","luxury","british"] },
+
+  { name:"Ocean Victory", tags:["white","charter","large","modern","steel"] },
+
+  { name:"Mayan Queen IV", tags:["white","classic","luxury","large","mexico"] },
+
+  { name:"Alfa Nero", tags:["dark","pool","charter","luxury","modern"] },
+
+  { name:"Project X", tags:["modern","sleek","charter","white","minimal"] },
+
+  { name:"Aquijo", tags:["sailing","performance","regatta","palma","elite"] },
+
+  { name:"Nero", tags:["black","modern","sleek","luxury","charter"] },
+
+  { name:"Cloud 9", tags:["white","charter","modern","balearic","sleek"] },
+
+  { name:"Ulysses", tags:["explorer","white","rugged","expedition","large"] },
+
+  { name:"Hampshire II", tags:["classic","white","charter","med","british"] },
+
+  { name:"Lionheart", tags:["british","luxury","white","classic","palma"] },
+
+  { name:"Lady S", tags:["modern","white","sleek","luxury","large"] },
+
+  { name:"Tranquility", tags:["spa","white","resort","luxury","charter"] },
+
+  { name:"Radiant", tags:["security","white","large","luxury","steel"] },
+
+  { name:"Symphony", tags:["white","feadship","modern","huge","charter"] },
+
+  { name:"Anna", tags:["explorer","white","massive","rugged","steel"] },
+
+  { name:"Phoenix 2", tags:["classic","artdeco","luxury","white","charter"] },
+
+  { name:"Boadicea", tags:["classic","white","british","luxury","large"] },
+
+  { name:"Christina O", tags:["historic","classic","iconic","med","vintage"] },
+
+  { name:"Nautilus", tags:["explorer","submarine","dark","rugged","expedition"] },
+
+  { name:"Dragon", tags:["sleek","modern","charter","white","fast"] },
+
+  { name:"Moonrise", tags:["minimal","modern","white","clean","feadship"] },
+
+  { name:"Amadea", tags:["luxury","white","large","modern","steel"] },
+
+  { name:"Solandge", tags:["charter","white","luxury","sleek","pool"] },
+
+  { name:"Lady Lara", tags:["white","luxury","modern","large","lürssen"] }
+
+];
