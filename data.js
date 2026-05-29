10b293e64c858cb92ebb2004695f993d77e2e156
ilsamoniquecarter <168980197+ilsamoniquecarter@users.noreply.github.com>
2026-05-29 17:00:44 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,40 @@
 const yachts = [
   {
+    name: "A",
+    deepPockets: "Andrey Melnichenko",
+    thumb: "images/a-diagram.svg",
+    yachtLink: "https://www.superyachtfan.com/yacht/a/",
+    ownerLink: "https://en.wikipedia.org/wiki/Andrey_Melnichenko"
+  },
+  {
+    name: "Azzam",
+    deepPockets: "Al Nahyan family",
+    thumb: "images/azzam-diagram.svg",
+    yachtLink: "https://www.superyachtfan.com/yacht/azzam/",
+    ownerLink: "https://en.wikipedia.org/wiki/Al_Nahyan_family"
+  },
+  {
+    name: "Black Pearl",
+    deepPockets: "Oleg Burlakov estate",
+    thumb: "images/black-pearl-diagram.svg",
+    yachtLink: "https://www.superyachtfan.com/yacht/black-pearl/",
+    ownerLink: "https://en.wikipedia.org/wiki/Black_Pearl_(yacht)"
+  },
+  {
+    name: "Dilbar",
+    deepPockets: "OWNERSHIP DISPUTED",
+    thumb: "images/dilbar-diagram.svg",
+    yachtLink: "https://www.superyachtfan.com/yacht/dilbar/",
+    ownerLink: "https://en.wikipedia.org/wiki/Dilbar_(yacht)"
+  },
+  {
+    name: "Eclipse",
+    deepPockets: "Roman Abramovich",
+    thumb: "images/eclipse-diagram.svg",
+    yachtLink: "https://www.superyachtfan.com/yacht/eclipse/",
+    ownerLink: "https://en.wikipedia.org/wiki/Roman_Abramovich"
+  },
+  {
     name: "Koru",
     deepPockets: "Jeff Bezos",
     thumb: "images/koru-diagram.svg",
@@ -14,32 +49,11 @@ const yachts = [
     ownerLink: "https://en.wikipedia.org/wiki/Mark_Zuckerberg"
   },
   {
-    name: "Eclipse",
-    deepPockets: "Roman Abramovich",
-    thumb: "images/eclipse-diagram.svg",
-    yachtLink: "https://www.superyachtfan.com/yacht/eclipse/",
-    ownerLink: "https://en.wikipedia.org/wiki/Roman_Abramovich"
-  },
-  {
-    name: "A",
-    deepPockets: "Andrey Melnichenko",
-    thumb: "images/a-diagram.svg",
-    yachtLink: "https://www.superyachtfan.com/yacht/a/",
-    ownerLink: "https://en.wikipedia.org/wiki/Andrey_Melnichenko"
-  },
-  {
-    name: "Sailing Yacht A",
-    deepPockets: "Andrey Melnichenko",
-    thumb: "images/sailing-yacht-a-diagram.svg",
-    yachtLink: "https://www.superyachtfan.com/yacht/sailing-yacht-a/",
-    ownerLink: "https://en.wikipedia.org/wiki/Andrey_Melnichenko"
-  },
-  {
-    name: "Dilbar",
-    deepPockets: "OWNERSHIP DISPUTED",
-    thumb: "images/dilbar-diagram.svg",
-    yachtLink: "https://www.superyachtfan.com/yacht/dilbar/",
-    ownerLink: "https://en.wikipedia.org/wiki/Dilbar_(yacht)"
+    name: "Octopus",
+    deepPockets: "Paul Allen",
+    thumb: "images/octopus-diagram.svg",
+    yachtLink: "https://www.superyachtfan.com/yacht/octopus/",
+    ownerLink: "https://en.wikipedia.org/wiki/Paul_Allen"
   },
   {
     name: "Rising Sun",
@@ -49,24 +63,10 @@ const yachts = [
     ownerLink: "https://en.wikipedia.org/wiki/David_Geffen"
   },
   {
-    name: "Octopus",
-    deepPockets: "Paul Allen",
-    thumb: "images/octopus-diagram.svg",
-    yachtLink: "https://www.superyachtfan.com/yacht/octopus/",
-    ownerLink: "https://en.wikipedia.org/wiki/Paul_Allen"
-  },
-  {
-    name: "Black Pearl",
-    deepPockets: "Oleg Burlakov estate",
-    thumb: "images/black-pearl-diagram.svg",
-    yachtLink: "https://www.superyachtfan.com/yacht/black-pearl/",
-    ownerLink: "https://en.wikipedia.org/wiki/Black_Pearl_(yacht)"
-  },
-  {
-    name: "Azzam",
-    deepPockets: "Al Nahyan family",
-    thumb: "images/azzam-diagram.svg",
-    yachtLink: "https://www.superyachtfan.com/yacht/azzam/",
-    ownerLink: "https://en.wikipedia.org/wiki/Al_Nahyan_family"
+    name: "Sailing Yacht A",
+    deepPockets: "Andrey Melnichenko",
+    thumb: "images/sailing-yacht-a-diagram.svg",
+    yachtLink: "https://www.superyachtfan.com/yacht/sailing-yacht-a/",
+    ownerLink: "https://en.wikipedia.org/wiki/Andrey_Melnichenko"
   }
 ];
